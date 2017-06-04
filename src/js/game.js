64ed5d85da218db5e8f523d47321363393f02513
sairoutine <sairoutine@gmail.com>
2017-06-04 13:09:44 +0900

    update

--- src/js/game.js
+++ src/js/game.js
@@ -1,6 +1,7 @@
 'use strict';
 var core = require('./hakurei').core;
 var util = require('./hakurei').util;
+var SceneStage = require('./scene/stage');
 
 var Game = function(canvas) {
 	core.apply(this, arguments);
@@ -9,6 +10,10 @@ util.inherit(Game, core);
 
 Game.prototype.init = function () {
 	core.prototype.init.apply(this, arguments);
+
+	this.addScene("stage", new SceneStage(this));
+
+	this.changeSceneWithLoading("stage");
 };
 
 module.exports = Game;
